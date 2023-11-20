5229e0b62310cc61b2e66d866a3c5354734e9546
aliaksei.chorny <aliaksei.chorny@revolut.com>
2023-11-20 23:24:44 +0100

    Final changes

--- lua/mol/plugins/lsp/lspconfig.lua
+++ lua/mol/plugins/lsp/lspconfig.lua
@@ -125,10 +125,33 @@ return {
 			filetypes = { "html", "typescriptreact", "javascriptreact", "css", "sass", "scss", "less", "svelte" },
 		})
 
+		local path = lspconfig.util.path
 		-- configure python server
+		local function get_python_path(workspace)
+			local match = vim.fn.glob(path.join(workspace, "poetry.lock"))
+			if match ~= "" then
+				local venv = vim.fn.trim(vim.fn.system("poetry env info -p"))
+				return path.join(venv, "bin", "python")
+			end
+
+			-- Find and use virtualenv in workspace directory.
+			for _, pattern in ipairs({ "*", ".*" }) do
+				local match = vim.fn.glob(path.join(workspace, pattern, "pyvenv.cfg"))
+				if match ~= "" then
+					return path.join(path.dirname(match), "bin", "python")
+				end
+			end
+
+			-- Fallback to system Python.
+			return vim.fn.exepath("python3") or vim.fn.exepath("python") or "python"
+		end
+
 		lspconfig["pyright"].setup({
 			capabilities = capabilities,
 			on_attach = on_attach,
+			before_init = function(_, config)
+				config.settings.python.pythonPath = get_python_path(config.root_dir)
+			end,
 		})
 
 		-- configure lua server (with special settings)
