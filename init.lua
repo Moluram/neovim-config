6f4373b192e1794a22d15f763aa98aeff3dcde0f
aliaksei.chorny <aliaksei.chorny@revolut.com>
2023-11-20 23:29:20 +0100

    Clear all

--- init.lua
+++ init.lua
@@ -1,5 +1,1 @@
-require("mol.core.options")
-require("mol.core.keymaps")
-require("mol.core.colorscheme")
-require("mol.lazy")
 
--- lua/mol/plugins/init.lua
+++
@@ -1,9 +0,0 @@
-return {
-	-- tmux windows navigator with c-lhjhh
-	"christoomey/vim-tmux-navigator",
-	"tpope/vim-surround",
-
-	"nvim-lua/plenary.nvim",
-	-- commenting with gs
-	"vim-scripts/ReplaceWithRegister",
-}
